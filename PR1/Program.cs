2b6109f010b4db18b8c6bb5f0801c8200cb4d757
Hawinar <egorchelnokov@bk.ru>
2022-12-03 10:42:04 +0300

    Добавлена поддержка метода зотого сечения

--- PR1/Program.cs
+++ PR1/Program.cs
@@ -4,39 +4,51 @@ namespace PR1
 {
     internal class Program
     {
-        //ПР 1: Методы оптимизации https://github.com/Hawinar
-
-        //С decimal, конечно, я переборчил, даже нашёл аналог Math, но на decimal https://github.com/raminrahimzada/CSharp-Helper-Classes/tree/master/Math/DecimalMath.
-        //Однако, было намного проще тестировать, зная, что числа будут 1 в 1 с калькулятором и даже не придётся задаваться вопросом,
-        //типа "Это формула неверная или double не смог удержать точное число?". Не думаю, что в этой программе возникнут проблемы только из-за типа данных decimal.
-        //Обычно, я стараюсь использовать более лёгкие типы данных, но раз программа только для холодного расчёта, эту задачу она должна выполнять как можно точнее.
-        //К тому же, тема довольно сложная и я не стал гадать, где какой тип данных понадобится.
-        //По поводу наименования переменных, их названия я с примеров тех взял, чтобы удобнее было переписывать.
+        //ПР 1: Методы одномерной оптимизации https://github.com/Hawinar Вариант: 8
 
         static void Main(string[] args)
         {
-            decimal eps = 0.00001M; //Допустимая погрешность
-            decimal a = 2;
-            decimal b = 6; //Кол-во также итераций повысится, если вы значение этой переменной повысите
+            double eps = 0.00001;
+            double a = 2;
+            double b = 5;
 
             Console.WriteLine("Метод дихотомии:\nМинимальное значение: " + dihot(a, b, eps, function(1)).Item1 + "\nИтераций: " + dihot(a, b, eps, function(1)).Item2 + "\n");
 
-            
-            Console.WriteLine("Метод Фибоначчи:\nМинимальное значение: " + OptFib(a, b, eps, function(1)).Item1 + "\nИтераций: " + OptFib(a, b, eps, function(1)).Item2 + 
+            Console.WriteLine($"Метод золотого сечения:\nМинимальное значение: {GoldenRation(a, b, eps).Item1} \nИтераций: {GoldenRation(a, b, eps).Item2}\n");
+            a = 2;
+            b = 5;
+            Console.WriteLine("Метод Фибоначчи:\nМинимальное значение: " + OptFib(a, b, eps, function(1)).Item1 + "\nИтераций: " + OptFib(a, b, eps, function(1)).Item2 +
                 "\nfmin: " + function(OptFib(a, b, eps, function(1)).Item1));
+            Console.ReadKey();
         }
-        static decimal function(decimal x)
+        static double function(double x)
         {
-            return DMath.Log(2 * x) * x + 2 * DMath.Sin(x) - 1;
-            //return DMath.Power((x + 1), (x / 2)) + DMath.Power((10 - x), 2); //Функция из примера (для проверки)
+            return Math.Log(2 * x) * x + 2 * Math.Sin(x) - 1;
+            //return Math.Pow((x + 1), (x / 2)) + Math.Pow((10 - x), 2); //Функция из примера (для проверки)
         }
-
-        static (decimal, int) dihot(decimal a, decimal b, decimal eps, decimal f)
+        static (double,int) GoldenRation(double a, double b, double eps)
         {
             int k = 0;
-            decimal c, f1, f2 = 0;
+            while (Math.Abs(b - a) > eps)
+            {
+                double x1 = b - (b - a) / 1.618;
+                double x2 = a + (b - a) / 1.618;
+                double y1 = function(x1);
+                double y2 = function(x2);
+                if (y1 >= y2)
+                    a = x1;
+                else
+                    b = x2;
+                k++;
+            }
+            return ((a + b) / 2,k);
+        }
+        static (double, int) dihot(double a, double b, double eps, double f)
+        {
+            int k = 0;
+            double c, f1, f2 = 0;
 
-            while (DMath.Abs(a - b) > eps)
+            while (Math.Abs(a - b) > eps)
             {
                 k++;
                 c = (a + b) / 2;
@@ -54,16 +66,16 @@ namespace PR1
             }
             return ((a + b) / 2, k);
         }
-        static (decimal, int) OptFib(decimal a, decimal b, decimal eps, decimal f)
+        static (double, int) OptFib(double a, double b, double eps, double f)
         {
             int k, i, s, j;
             k = i = s = j = 0;
-            decimal x, fx, x1, fx1, h;
+            double x, fx, x1, fx1, h;
             x = fx = x1 = fx1 = h = 0;
-            
-            decimal N = (b - a) / eps;
-            decimal[] R = new decimal[(int)N];
-            decimal Fib(decimal N)
+
+            double N = (b - a) / eps;
+            double[] R = new double[(int)N];
+            double Fib(double N)
             {
                 R[0] = 1;
                 R[1] = 1;
@@ -76,14 +88,14 @@ namespace PR1
                 return i - 1;
             }
             s = (int)Fib(N);
-            R = new decimal[(int)N];
+            R = new double[(int)N];
             i = 0;
-            decimal Num(decimal k)
+            double Num(double k)
             {
                 R[0] = 1;
                 R[1] = 1;
                 i = 2;
-                while (i<=k)
+                while (i <= k)
                 {
                     R[i] = R[i - 1] + R[i - 2];
                     i++;
@@ -114,11 +126,11 @@ namespace PR1
                     x = x1;
                     fx = fx1;
                     x1 = b - h * Num(s - j - 2);
-                    fx1=function(x1);
+                    fx1 = function(x1);
                 }
                 j++;
             }
-            return ((x+x1)/2, j);
+            return ((x + x1) / 2, j);
         }
     }
 }
